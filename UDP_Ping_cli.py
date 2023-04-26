102e5c3cc33272516eecd13ee6463966da3e9ea7
VitorLameirao <vitorlameirao10@gmail.com>
2023-04-25 22:30:34 -0400

    feat: adicionado métodos necessários

--- UDP_Ping_cli.py
+++ UDP_Ping_cli.py
@@ -10,10 +10,10 @@ timeout = 1 # in second
 # Create UDP client socket
 # Note the use of SOCK_DGRAM for UDP datagram packet
 #/**/ /* 1. Cria o socket UDP */
-
+client_socket = socket(AF_INET, SOCK_DGRAM)
 # Set socket timeout as 1 second
 #/**/ /* Seta o timeout de 1s para o socket */
-
+client_socket.settimeout(timeout)
 
 # Command line argument is a string, change the port into integer
 port = int(port)  
@@ -25,15 +25,19 @@ while seqNum < 10:
 	seqNum += 1
 	# Format the message to be sent
 	data = "Ping " + str(seqNum) + " " + time.asctime()
-    
+
 	try:
 	# Sent time
 		RTTs = time.time()  #time in seconds
 	# Send the UDP packet with the ping message
 	#/**/ /* 2. Envia os dados para o servidor */
+		client_socket.sendto(data.encode(), (host, port))
 
 	# Receive the server response
 	#/**/ /* 3. Recebe o reply do servidor */
+
+	message, address = client_socket.recvfrom(1024)
+    	print("Mensagem recebida: ", message.decode())
 
 	# Received time
 		RTTr = time.time()
@@ -41,7 +45,7 @@ while seqNum < 10:
 		print("Reply from " + address[0] + ": " + message.decode())       
 	# Round trip time is the difference between sent and received time
 	#/**/ /* 4. Apresenta o RTT na tela */
-
+		print("RTT: ", RTTr - RTTs)
 	except:
 		# Server does not respond
 	        # Assume the packet is lost
@@ -50,7 +54,7 @@ while seqNum < 10:
 
 # Close the client socket
 #/**/ /* 5. Fecha o socket */
-
+client_socket.close()
  
 
 
